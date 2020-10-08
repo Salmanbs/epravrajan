e83754cfbe368e10225cf9ecc3bcd7a2716891b7
SHARUQ007 <58297752+SHARUQ007@users.noreply.github.com>
2020-10-08 20:39:59 +0530

    first commit

--- app.js
+++ app.js
@@ -1,87 +1,18 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-var worker = [{
-  name: "",
-  work: ""
-}]
-const wmobnum = [];
-const wpswrd = [];
-const bmobnum = [];
-const bpswrd = [];
-
-const wfname = [];
-const wdis = [];
-const wage = [];
-const wmail = [];
-const wphnum = [];
-const wpadd = [];
-const wadhar = [];
-const weduq = [];
-const wlivin = [];
-const wbank = [];
-const wexper = [];
-const wlang = [];
-const wstate = [];
-const wjob = [];
-const wct = [];
-const wcoach = [];
-const wscheme = [];
-const wmarry = [];
-
-const sfname = [];
-const sdis = [];
-const sage = [];
-const smail = [];
-const sphnum = [];
-const spadd = [];
-const sadhar = [];
-const seduq = [];
-const slivin = [];
-const sbank = [];
-const sexper = [];
-const slang = [];
-const sstate = [];
-const sjob = [];
-const sct = [];
-const scoach = [];
-const sscheme = [];
-const schils = [];
-
-const cnum = [];
-var m, b;
-
-const bven=[];
-const breg=[];
-const bcity=[];
-const bstat=[];
-const bweb=[];
-const bphnum=[];
-const blab=[];
-const bmig=[];
-const bnum=[];
-const bacco=[];
-const bben=[];
-const bcont=[];
-const baddr=[];
-const bpin=[];
-const bdis=[];
-const bemail=[];
-const bexp=[];
-const bwork=[];
-const bjtype=[];
-const bsal=[];
-const bfood=[];
-const btype=[];
-const bacco=[];
-const bjosp=[];
-const bedinst=[];
+mongoose.connect("mongodb://localhost:27017/epravdb",{useNewUrlParser: true});
 app.use(express.static("public"));
+const workerSchema={
+  name:String,
+};
+const Worker=mongoose.model("Worker",workerSchema);
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -104,71 +35,6 @@ app.post("/register.html", function(req, res) {
 
 });
 app.post("/register_mi.html", function(req, res) {
-  wfname.push(req.body.fname[0]);
-  wdis.push(req.body.dist[0]);
-  wage.push(req.body.age[0]);
-  wmail.push(req.body.mail[0]);
-  wphnum.push(req.body.phnum[0]);
-  wpadd.push(req.body.pa[0]);
-  wadhar.push(req.body.an[0]);
-  weduq.push(req.body.heq[0]);
-  if (req.body.work[0] === "other") {
-    wlivin.push(req.body.other[0]);
-  } else {
-    wlivin.push(req.body.work[0]);
-  }
-  wbank.push(req.body.bad[0]);
-  wexper.push(req.body.xp[0]);
-  wlang.push(req.body.lang[0]);
-  if (req.body.responce_state === "Yes") {
-    wstate.push(req.body.state[0]);
-  } else {
-    wstate.push("India");
-  }
-  wjob.push(req.body.responce_job[0]);
-  wct.push(req.body.responce_covid[0]);
-  if (req.body.req.body.responce_skill[0] === "Yes") {
-    wcoach.push(req.body.skill[0]);
-  } else {
-    wcoach.push("No");
-  }
-  wscheme.push(req.body.responce_scheme[0]);
-  wmarry.push(req.body.responce_marriage[0]);
-
-  sfname.push(req.body.fname[1]);
-  sdis.push(req.body.dist[1]);
-  sage.push(req.body.age[1]);
-  smail.push(req.body.mail[1]);
-  sphnum.push(req.body.phnum[1]);
-  spadd.push(req.body.pa[1]);
-  sadhar.push(req.body.an[1]);
-  seduq.push(req.body.heq[1]);
-  if (req.body.work[1] === "other") {
-    slivin.push(req.body.other[1]);
-  } else {
-    slivin.push(req.body.work[1]);
-  }
-  sbank.push(req.body.bad[1]);
-  sexper.push(req.body.xp[1]);
-  slang.push(req.body.lang[1]);
-  if (req.body.responce_state === "Yes") {
-    sstate.push(req.body.state[1]);
-  } else {
-    sstate.push("India");
-  }
-  sjob.push(req.body.responce_job[1]);
-  sct.push(req.body.responce_covid[1]);
-  if (req.body.req.body.responce_skill[1] === "Yes") {
-    scoach.push(req.body.skill[1]);
-  } else {
-    scoach.push("No");
-  }
-  sscheme.push(req.body.responce_scheme[1]);
-  schils.push(req.body.responce_marriage[1]);
-
-  cnum.push(req.body.child);
-
-
 
 
   res.sendFile(__dirname + "/dash_mi.html");
